4a9918d5f84cd9a7503c335f80514a9ab64f71fa
smakto <165059651+smakto@users.noreply.github.com>
2024-10-09 21:39:46 +0300

    Testing configuration setup.

---
+++ iamjunior/jest.config.js
@@ -0,0 +1,18 @@
+export default {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "^.+\\.svg$": "jest-transformer-svg",
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
+  coverageReporters: ["text", "text-summary"],
+};
