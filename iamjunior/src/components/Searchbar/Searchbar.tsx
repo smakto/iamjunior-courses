662f08468d52b60da35798f917682a8f0470222a
smakto <165059651+smakto@users.noreply.github.com>
2024-11-08 20:07:48 +0200

    Searchbar functionality implemented.

--- iamjunior/src/components/Searchbar/Searchbar.tsx
+++ iamjunior/src/components/Searchbar/Searchbar.tsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 import styles from "./Searchbar.module.scss";
 
-export function Searchbar() {
+type SearchBarProps = {
+  searchService: (input: string) => void;
+  resetSearch: () => void;
+};
+
+export const Searchbar: React.FC<SearchBarProps> = ({
+  searchService,
+  resetSearch,
+}) => {
   const [searchInput, setSearchInput] = useState<string>();
 
   function handleSearchInput(event: React.ChangeEvent<HTMLInputElement>) {
@@ -9,7 +17,13 @@ export function Searchbar() {
   }
 
   return (
-    <form className={styles.searchForm}>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (searchInput) searchService(searchInput);
+      }}
+      className={styles.searchForm}
+    >
       <input
         type="text"
         name="search"
@@ -17,6 +31,9 @@ export function Searchbar() {
         onChange={handleSearchInput}
       ></input>
       <button type="submit"> Search</button>
+      <button type="button" onClick={resetSearch}>
+        Reset search
+      </button>
     </form>
   );
-}
+};
--- iamjunior/src/pages/Home/HomePage.tsx
+++ iamjunior/src/pages/Home/HomePage.tsx
@@ -2,20 +2,39 @@ import { Searchbar } from "../../components/Searchbar/Searchbar";
 import { CategoryField } from "../../components/CategoryField/CategoryField";
 import { ServiceContainer } from "../../components/ServiceCards/ServiceContainer";
 import { useServices } from "../../hooks/useServices";
+import { useState } from "react";
+import { Service } from "@/types/types-service";
 
 export const HomePage: React.FC = () => {
   const { data, error, isLoading } = useServices();
+  const [searchedData, setSearchedData] = useState<Service[] | null>(null);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error loading users</div>;
+
+  function searchService(input: string) {
+    if (data) {
+      const searchResults = data.filter(
+        (item) =>
+          item.category.toLowerCase().includes(input.toLowerCase()) ||
+          item.name.toLowerCase().includes(input.toLowerCase()) ||
+          item.contactPerson.toLowerCase().includes(input.toLowerCase())
+      );
+      setSearchedData(searchResults);
+    }
+  }
+
+  function resetSearch() {
+    setSearchedData(null);
+  }
 
   return (
     <>
       <h1>Find services near you</h1>
       <h2>Expore best services in your area and beyond</h2>
-      <Searchbar />
+      <Searchbar searchService={searchService} resetSearch={resetSearch} />
       <CategoryField />
-      {data && <ServiceContainer servicesData={data} />}
+      {data && <ServiceContainer servicesData={searchedData || data} />}
     </>
   );
 };
